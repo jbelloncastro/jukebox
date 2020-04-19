a95cc8c1dcdc539e9a61e98b048a2ea9035a0a12
Jorge Bellon-Castro <jbelloncastro@gmail.com>
2020-04-19 15:33:22 +0100

    Move files to subdirectories based on functionality

---
+++ dbus/signals.py
@@ -0,0 +1,65 @@
+
+from jeepney import DBusAddress
+from jeepney.bus_messages import MatchRule
+
+
+class PropertiesChanged(MatchRule):
+    interface = 'org.freedesktop.DBus.Properties'
+
+    def __init__(self,
+            object_path='/org/mpris/MediaPlayer2',
+            bus_name='org.mpris.MediaPlayer2.vlc'):
+        super().__init__(type='signal',
+                         sender='org.mpris.MediaPlayer2.vlc',
+                         interface=PropertiesChanged.interface,
+                         path=object_path,
+                         member='PropertiesChanged')
+
+class TrackListReplaced(MatchRule):
+    interface = 'org.mpris.MediaPlayer2.TrackList'
+
+    def __init__(self,
+            object_path='/org/mpris/MediaPlayer2',
+            bus_name='org.mpris.MediaPlayer2.vlc'):
+        super().__init__(type='signal',
+                         sender='org.mpris.MediaPlayer2.vlc',
+                         interface=PropertiesChanged.interface,
+                         path=object_path,
+                         member='TrackListReplaced')
+
+class TrackAdded(MatchRule):
+    interface = 'org.mpris.MediaPlayer2.TrackList'
+
+    def __init__(self,
+            # object_path='/org/mpris/MediaPlayer2',
+            bus_name='org.mpris.MediaPlayer2.vlc'):
+        super().__init__(type='signal',
+                         sender='org.mpris.MediaPlayer2.vlc',
+                         interface=PropertiesChanged.interface,
+                         # path=object_path,
+                         member='TrackAdded')
+
+class TrackRemoved(MatchRule):
+    interface = 'org.mpris.MediaPlayer2.TrackList'
+
+    def __init__(self,
+            object_path='/org/mpris/MediaPlayer2',
+            bus_name='org.mpris.MediaPlayer2.vlc'):
+        super().__init__(type='signal',
+                         sender='org.mpris.MediaPlayer2.vlc',
+                         interface=PropertiesChanged.interface,
+                         path=object_path,
+                         member='TrackRemoved')
+
+class TrackMetadataChanged(MatchRule):
+    interface = 'org.mpris.MediaPlayer2.TrackList'
+
+    def __init__(self,
+            object_path='/org/mpris/MediaPlayer2',
+            bus_name='org.mpris.MediaPlayer2.vlc'):
+        super().__init__(type='signal',
+                         sender='org.mpris.MediaPlayer2.vlc',
+                         interface=PropertiesChanged.interface,
+                         path=object_path,
+                         member='TrackMetadataChanged')
+
---
+++ queue/track_metadata.py
@@ -0,0 +1,28 @@
+class Track:
+    def __init__(self, tid, title, caption, tags, url):
+        self.id = tid
+        self.title = title
+        self.caption = caption
+        self.tags = tags
+        self.url = url
+
+
+class TrackMetadata:
+    "VLC track metadata"
+
+    def __init__(self, trackId, uri, length):
+        self.trackId = (trackId,)
+        self.uri = uri
+        self.length = length
+
+
+def parseTrackMetadata(reply):
+    # Track metadata is an array of string-variant pairs, where a variant is a
+    # tuple with string-encoded type and the value
+    result = {}
+    for key, variant in reply:
+        _, value = variant  # we don't really need the type
+        result[key] = value
+    return TrackMetadata(
+        result["mpris:trackid"], result["xesam:url"], result["mpris:length"]
+    )
