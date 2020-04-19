85f9f7534f08c80a38c1a48eb8a00637b5e207c9
Jorge Bellon-Castro <jbelloncastro@gmail.com>
2020-04-19 15:37:08 +0100

    Include video id in search results and generate hash of tracklist

--- queue/queue.py
+++ queue/queue.py
@@ -8,21 +8,42 @@ from search import YouTubeFinder
 # Talk with DBus daemon itself
 from jeepney.integrate.blocking import connect_and_authenticate
 
+import hashlib
+
 playlist = True
+list_hash = hashlib.sha1()
 
 with connect_and_authenticate(bus="SESSION") as connection:
     finder = YouTubeFinder()
-    result = finder.search("they are taking the hobbits to isenhart")
+    tracklist = Proxy(TrackList(), connection)
+    player = Proxy(Player(), connection)
+    playerProperties = Proxy(PlayerProperties(), connection)
 
-    if not playlist:
-        player = Proxy(Player(), connection)
-        reply = player.OpenUri(result.url)
-    else:
-        # Start playing this one
-        setCurrent = False
-        # Insert at the beginning of the list
-        track = "/org/mpris/MediaPlayer2/TrackList/NoTrack"
+    try:
+        while True:
+            query = input("ytsearch: ")
+            if len(query) > 0:
+                result = finder.search(query)
+                url = result.url
+                list_hash.update(str.encode(result.id))
+                print("Playing url: {}, digest: {}".format(url, list_hash.hexdigest()))
+    
+                # Insert at the beginning of the list
+                track = "/org/mpris/MediaPlayer2/TrackList/Append"
+                # track = "/org/mpris/MediaPlayer2/TrackList/NoTrack"
+                # Insert after element
+                # track = "/org/videolan/vlc/playlist/0"
+                # track = ""
 
-        tracklist = Proxy(TrackList(), connection)
-        reply = tracklist.AddTrack(result.url, track, setCurrent)
-        print(reply)
+                # Resume playing if necessary
+                # Alternative: player.GoTo('/org/videolan/vlc/playlist/{id}')
+                status = playerProperties.PlaybackStatus()[0]
+                resume = status == ('s', 'Stopped')
+                print("Status: {}, Resume: {}".format(status, resume))
+                reply = tracklist.AddTrack(url, track, resume)
+                if resume:
+                    player.Play()
+    except EOFError:
+        pass
+    except KeyboardInterrupt:
+        pass
--- search/search.py
+++ search/search.py
@@ -1,37 +1,34 @@
-
 # Perform YouTube video searches
 import youtube_dl
 
-
-class Result:
-    def __init__(self, title, caption, tags, url):
-        self.title = title
-        self.caption = caption
-        self.tags = tags
-        self.url = url
+from track_metadata import Track
 
 
 class YouTubeFinder:
     def __init__(self):
         self.query_format = "ytsearch1:{}"
 
-        options = {
-            'format': 'bestaudio/best',
-            'simulate': True
-        }
+        options = {"format": "bestaudio/best", "simulate": True}
 
         self.downloader = youtube_dl.YoutubeDL(options)
 
     def search(self, query):
+        "Search YouTube for the first result and returns information "
+        "for the highest quality, audio-only result"
+
         def audioBitrate(f):
-            return f['abr'] if f['vcodec'] == 'none' else 0
+            "Sorts by audio bitrate prioritizing audio-only results"
+            return f["abr"] if f["vcodec"] == "none" else 0
 
         query = self.query_format.format(query)
         results = self.downloader.extract_info(query, False)
-        result = results['entries'][0]
-        selected = max(result['formats'], key=audioBitrate)
+        result = results["entries"][0]
+        selected = max(result["formats"], key=audioBitrate)
 
-        return Result(result['title'],
-                      result['thumbnail'],
-                      result['tags'],
-                      selected['url'])
+        return Track(
+            result["id"],
+            result["title"],
+            result["thumbnail"],
+            result["tags"],
+            selected["url"],
+        )
